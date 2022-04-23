00612012117dea82ba17d10895e30cd42d30c883
nilanchal patra <nilanchal.patra3777@gmail.com>
2022-04-23 19:54:58 +0530

    Bug fixes

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,14 +1,9 @@
 import React from "react";
 import "@/styles/globals.css";
-import App from "next/app";
 import store from "@/store";
 
-class MyApp extends App {
-  render() {
-    const { Component, pageProps } = this.props;
-
-    return <Component {...pageProps} />;
-  }
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
 
 export default store.withRedux(MyApp);
