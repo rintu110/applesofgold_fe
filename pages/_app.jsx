ad882cf438a5d99d2c056c39fc59c51c600c2dfb
nilanchal patra <nilanchal.patra3777@gmail.com>
2022-04-23 19:50:07 +0530

    Bug fixes

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -4,14 +4,6 @@ import App from "next/app";
 import store from "@/store";
 
 class MyApp extends App {
-  static async getInitialProps({ Component, ctx }) {
-    let pageProps = {};
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx);
-    }
-    return pageProps;
-  }
-
   render() {
     const { Component, pageProps } = this.props;
 
