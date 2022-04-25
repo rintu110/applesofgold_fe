c65ad0c63d970f545ee28e3480dc3b68e875f695
nilanchal patra <nilanchal.patra3777@gmail.com>
2022-04-25 18:13:36 +0530

    Icon Changed

--- src/config/index.jsx
+++ src/config/index.jsx
@@ -1,5 +1,6 @@
 const UNIVERSAL = {
-  BASEURL: "http://localhost:8000/applesofgold/",
+  BASEURL: "https://applesofgoldbe.herokuapp.com/",
+  BASEURL1: "http://localhost:8000/applesofgold/",
 };
 
 export default UNIVERSAL;
