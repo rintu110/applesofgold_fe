9a39fd404df65671d011f62d834f4988efcfb1b9
nilanchal patra <nilanchal.patra3777@gmail.com>
2022-04-25 14:56:47 +0530

    deploying frontend

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  pageExtensions: ["jsx", "js"],
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import React from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import ServerStyleSheets from "@mui/styles/ServerStyleSheets";
 
-class AppleOfGoldDocument extends Document {
+class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const sheets = new ServerStyleSheets();
 
@@ -51,4 +51,4 @@ class AppleOfGoldDocument extends Document {
   }
 }
 
-export default AppleOfGoldDocument;
+export default MyDocument;
