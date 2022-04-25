90035ee5ac29ac9d23caa7e308519f9bb53ab8f3
nilanchal patra <nilanchal.patra3777@gmail.com>
2022-04-25 16:49:37 +0530

    deploying frontend

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ["jsx", "js"],
+  pageExtensions: ["jsx"],
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
