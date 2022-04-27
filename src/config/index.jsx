1d78f30c78c2ccdabda9be534c0344edfc209c93
nilanchal patra <nilanchal.patra3777@gmail.com>
2022-04-27 23:16:27 +0530

    Bug fixes

--- src/config/index.jsx
+++ src/config/index.jsx
@@ -1,6 +1,6 @@
 const UNIVERSAL = {
-  BASEURL1: "https://applesofgoldbe.herokuapp.com/applesofgold/",
-  BASEURL: "http://localhost:8000/applesofgold/",
+  BASEURL: "https://applesofgoldbe.herokuapp.com/applesofgold/",
+  BASEURL1: "http://localhost:8000/applesofgold/",
 };
 
 export default UNIVERSAL;
